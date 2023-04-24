577e7274cd69174fe38204239aa12fa818d322cc
Gabriel Ribeiro <gabrieldeoculos@hotmail.com>
2023-04-24 13:26:48 -0300

    Update

---
+++ aula013.py
@@ -0,0 +1,16 @@
+for c in range(0,5):
+    i = int(input("Digite um numero: "))
+print("Fim")
+"""i = int(input("Inicio: "))
+f = int(input("Fim: "))
+p = int(input("Passos: "))
+for c in range(i, f+1, p):
+    print(c)
+print("Fim")"""
+"""for c in range(1,11): #Contando Normal 1 ate 10
+    print(c)
+print("Fim")"""
+"""for c in range(6, 0, -1): #Contando de tras pra frente 6 ate 1
+    print(c)"""
+"""for c in range(0, 11, 3): #Contando 0 ate 10 porem pulando de 2 em 2
+    print(c)"""
